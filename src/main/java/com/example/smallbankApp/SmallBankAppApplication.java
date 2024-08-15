dd0687b1fa7ccd346684db79ca77dfa111ba208c
Daniel Opara <oparadaniv@gmail.com>
2024-08-15 16:47:42 +0100

    adding servelet

--- src/main/java/com/example/smallbankApp/SmallBankAppApplication.java
+++ src/main/java/com/example/smallbankApp/SmallBankAppApplication.java
@@ -2,9 +2,15 @@ package com.example.smallbankApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmallBankAppApplication {
+public class SmallBankAppApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmallBankAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmallBankAppApplication.class, args);
